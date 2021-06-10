ef58fe1d40e40f1f78f2721e8027b6d618ff392f
firstsarcasm@gmail.com <18007RX0>
2021-06-10 22:50:53 +0300

    update spec;

--- SPECIFICATION.md
+++ SPECIFICATION.md
@@ -1,31 +1,48 @@
 # Тех. задание
 
-1. GET /set-amount
-query параметры:
-  Название: value  
-  Тип: double
+1. GET /set-amount  
+   ```
+    query параметры:
+   ```
+   | Название      | Тип       |
+   | :-----------: |:---------:|
+   | value         | double    |
+   
+   Сценарий:  
+   1. Метод принимает число(value) на вход  
+   2. Устанавливает значение amount пользователя равное value    
+   3. Возвращает в ответе json вида:  
+   ```json
+   {  
+    "result":"Your amount of money is 10"  
+   }  
+   ```  
+---
+2. GET /get-amount   
 Сценарий:
-1. Метод принимает число(value) на вход
-2. Устанавливает значение amount пользователя равное value
-3. Возвращает в ответе json вида
-{
- "result":"Your amount of money is 10"
-}
-  
-2. GET /get-amount
-Сценарий:
-1. Метод возвращает значение amount
-2. Возвращает в ответе json вида
-{
- "result":"Your amount of money is 10"
-}
+   1. Метод возвращает значение amount
+   2. Возвращает в ответе json вида:
+   ```json
+   {
+    "result":"Your amount of money is 10"
+   }
+   ```
 
+---
 2. GET /add-one
 Сценарий:
-1. Метод добавляет единицу к amount
-2. Возвращает в ответе json вида
-{
- "result":"Your amount of money is 11"
-}
-  
-result содержит текст Your amount of money is + значение параметра value, переданного в запросе
+   1. Метод добавляет единицу к amount
+   2. Возвращает в ответе json вида:
+   ```json
+   {
+    "result":"Your amount of money is 11"
+   }
+   ```  
+
+result содержит текст Your amount of money is + значение параметра "value"
+
+Значение amount по умолчанию равно 0
+
+##Запуск приложения
+java -jar server-*version*-.jar  
+Порт: 8080
--- src/main/java/some/testme/server/PaymentController.java
+++ src/main/java/some/testme/server/PaymentController.java
@@ -7,33 +7,40 @@ import some.testme.server.dto.PaymentResult;
 
 import java.math.BigDecimal;
 
+//todo add spring security
+//todo add validation
+//todo add correlationId
+//todo add swagger
+//todo add actuator
 @RestController
 public class PaymentController {
-    private int value = 0;
+	private final ThreadLocal<Integer> value = ThreadLocal.withInitial(() -> 1);
+	//todo fix with the solution
+//	private int value = 0;
 
-    @GetMapping("/set-amount")
-    public PaymentResult setAmount(
-            @RequestParam int value
-    ) {
-        this.value = value;
-        BigDecimal BDValue = BigDecimal.valueOf(value);
-        if (BDValue == null) {
-            if (BDValue.abs().multiply(BigDecimal.TEN)
-                    .compareTo(BigDecimal.valueOf(50000)) >= 0 ) {
-                BDValue = BDValue.add(BigDecimal.ONE);
-            }
-        }
-        return new PaymentResult("Your amaunt of money now is " +  BDValue);
-    }
+	@GetMapping("/set-amount")
+	public PaymentResult setAmount(
+			@RequestParam int value
+	) {
+		this.value.set(value);
+		BigDecimal BDValue = BigDecimal.valueOf(value);
+		if (BDValue != null) {
+			if (BDValue.abs().multiply(BigDecimal.TEN)
+					.compareTo(BigDecimal.valueOf(50)) >= 0) {
+				BDValue = BDValue.add(BigDecimal.ONE);
+			}
+		}
+		return new PaymentResult("Your amaunt of money now is " + BDValue);
+	}
 
-    @GetMapping("/get-amount")
-    public PaymentResult getAmount() {
-        return new PaymentResult("Your amaunt of money now is " +  this.value);
-    }
+	@GetMapping("/get-amount")
+	public PaymentResult getAmount() {
+		return new PaymentResult("Your amaunt of money now is " + this.value.get());
+	}
 
-    @GetMapping("/add-one")
-    public PaymentResult addOne() {
-        value++;
-        return new PaymentResult("Your amaunt of money now is " +  this.value);
-    }
+	@GetMapping("/add-one")
+	public PaymentResult addOne() {
+		value.set(value.get() + 1);
+		return new PaymentResult("Your amaunt of money now is " + this.value.get());
+	}
 }
