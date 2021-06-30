739885fac4cc5e08242a0d15183a9e94087d3757
firstsarcasm@gmail.com <18007RX0>
2021-06-30 20:36:32 +0300

    fix initial value

--- src/main/java/some/testme/server/PaymentController.java
+++ src/main/java/some/testme/server/PaymentController.java
@@ -14,33 +14,25 @@ import java.math.BigDecimal;
 //todo add actuator
 @RestController
 public class PaymentController {
-	private final ThreadLocal<Integer> value = ThreadLocal.withInitial(() -> 0);
-	//todo fix with the solution
-//	private int value = 0;
+//	private final ThreadLocal<Integer> value = ThreadLocal.withInitial(() -> 0);
+	private int value = 0;
 
 	@GetMapping("/set-amount")
 	public PaymentResult setAmount(
 			@RequestParam int value
 	) {
-		this.value.set(value);
-		BigDecimal BDValue = BigDecimal.valueOf(value);
-		if (BDValue != null) {
-			if (BDValue.abs().multiply(BigDecimal.TEN)
-					.compareTo(BigDecimal.valueOf(50)) >= 0) {
-				BDValue = BDValue.add(BigDecimal.ONE);
-			}
-		}
-		return new PaymentResult("Your amaunt of money now is " + BDValue);
+		this.value = value;
+		return new PaymentResult("Your amaunt of money now is " + value);
 	}
 
 	@GetMapping("/get-amount")
 	public PaymentResult getAmount() {
-		return new PaymentResult("Your amaunt of money now is " + this.value.get());
+		return new PaymentResult("Your amaunt of money now is " + this.value);
 	}
 
 	@GetMapping("/add-one")
 	public PaymentResult addOne() {
-		value.set(value.get() + 1);
-		return new PaymentResult("Your amaunt of money now is " + this.value.get());
+		this.value++;
+		return new PaymentResult("Your amaunt of money now is " + this.value);
 	}
 }
