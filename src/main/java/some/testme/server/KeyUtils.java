416debf389e6541a73196c401bacc20d875569d1
firstsarcasm@gmail.com <18007RX0>
2021-07-04 10:23:44 +0300

    clean

--- src/main/java/some/testme/server/KeyUtils.java
+++ src/main/java/some/testme/server/KeyUtils.java
@@ -1,9 +1,7 @@
 package some.testme.server;
 
-import lombok.AccessLevel;
 import lombok.NoArgsConstructor;
 import some.testme.server.dto.User;
-import some.testme.server.entity.UserEntity;
 
 import java.nio.charset.StandardCharsets;
 import java.util.Base64;
@@ -20,10 +18,4 @@ public class KeyUtils {
 		return MY_SECRET_KEY_FOR_BASE_64 + key;
 	}
 
-//	public static String extractKey(UserEntity user) {
-//		user.getKey()
-//		byte[] keyBytes = (user.getUsername() + "_" + user.getEmail() + "_" + user.getPwd()).getBytes(StandardCharsets.UTF_8);
-//		String key = new String(Base64.getEncoder().encode(keyBytes));
-//		return MY_SECRET_KEY_FOR_BASE_64 + key;
-//	}
 }
