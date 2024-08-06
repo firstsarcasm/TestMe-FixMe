9c07efbc9f4c3b8f2a3e128bfb8d5535b355267e
firstsarcasm <som2e@exam2ple.com>
2024-08-06 15:19:05 +0300

    fix get-token 'key' bug: https://rpg.kaiten.ru/space/372739/card/36798607;

--- src/main/java/some/testme/server/KeyUtils.java
+++ src/main/java/some/testme/server/KeyUtils.java
@@ -13,7 +13,7 @@ public class KeyUtils {
 	private static final String MY_SECRET_KEY_FOR_BASE_64 = "SE";
 
 	public static String extractKey(User user) {
-		byte[] keyBytes = (user.getPwd()).getBytes(StandardCharsets.UTF_8);
+		byte[] keyBytes = (user.getUsername() + "_" + user.getEmail() + "_" + user.getPwd()).getBytes(StandardCharsets.UTF_8);
 		String key = new String(Base64.getEncoder().encode(keyBytes));
 		return MY_SECRET_KEY_FOR_BASE_64 + key;
 	}
