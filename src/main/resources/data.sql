fad9a2b93eeccfb5333671653a85a466cc33079f
firstsarcasm <som2e@exam2ple.com>
2024-08-12 11:15:28 +0300

    fix 'amount' constraint bug: https://rpg.kaiten.ru/space/372739/card/36936332;

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -5,7 +5,7 @@ CREATE TABLE if not exists user
     id       BIGINT AUTO_INCREMENT PRIMARY KEY,
     username varchar2 NOT NULL UNIQUE,
     email    varchar2 NOT NULL UNIQUE,
-    amount   decimal  NOT NULL UNIQUE,
+    amount   decimal  NOT NULL,
     key      varchar2 NOT NULL UNIQUE
 );
 
