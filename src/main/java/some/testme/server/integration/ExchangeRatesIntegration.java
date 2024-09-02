6e748b7f1df57e639f0d01cde55261abf650d302
firstsarcasm <som2e@exam2ple.com>
2024-09-02 15:02:06 +0300

    move exchange-log url to application.properties: https://rpg.kaiten.ru/space/372739/card/37967965

--- src/main/java/some/testme/server/integration/ExchangeRatesIntegration.java
+++ src/main/java/some/testme/server/integration/ExchangeRatesIntegration.java
@@ -1,6 +1,7 @@
 package some.testme.server.integration;
 
 import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.lang.Nullable;
 import org.springframework.stereotype.Component;
 import org.springframework.web.client.RestTemplate;
@@ -15,8 +16,11 @@ public class ExchangeRatesIntegration {
 
 	private final RestTemplate exchangeRatesRestTemplate;
 
+	@Value("${integration.exchange-rate.url}")
+	private String exchangeRatesUrl;
+
 	public @Nullable Double getUsdRate() {
-		ExchangeRatesResponse result = exchangeRatesRestTemplate.getForObject("https://www.cbr-xml-daily.ru/latest.js", ExchangeRatesResponse.class);
+		ExchangeRatesResponse result = exchangeRatesRestTemplate.getForObject(exchangeRatesUrl, ExchangeRatesResponse.class);
 		return Optional.ofNullable(result)
 				.map(ExchangeRatesResponse::getRates)
 				.map(Rates::getUsd)
