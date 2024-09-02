7d28dab29162cbec7571b1e664c13c1fe68e0b66
firstsarcasm <som2e@exam2ple.com>
2024-09-02 14:58:13 +0300

    add more logs: https://rpg.kaiten.ru/space/372739/card/37969452

--- src/main/java/some/testme/server/config/IntegrationConfig.java
+++ src/main/java/some/testme/server/config/IntegrationConfig.java
@@ -1,10 +1,14 @@
 package some.testme.server.config;
 
+import org.springframework.boot.web.client.RestTemplateBuilder;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.MediaType;
+import org.springframework.http.client.BufferingClientHttpRequestFactory;
+import org.springframework.http.client.HttpComponentsClientHttpRequestFactory;
 import org.springframework.http.converter.json.MappingJackson2HttpMessageConverter;
 import org.springframework.web.client.RestTemplate;
+import org.zalando.logbook.spring.LogbookClientHttpRequestInterceptor;
 
 import java.util.List;
 
@@ -18,11 +22,14 @@ public class IntegrationConfig {
 	private static final List<MediaType> APPLICATION_JAVASCRIPT_MEDIA_TYPE = singletonList(new MediaType(APPLICATION, JAVASCRIPT));
 
 	@Bean(name = "exchangeRatesRestTemplate")
-	public RestTemplate restTemplate() {
-		RestTemplate restTemplate = new RestTemplate();
+	public RestTemplate restTemplate(LogbookClientHttpRequestInterceptor interceptor) {
 		MappingJackson2HttpMessageConverter converter = new MappingJackson2HttpMessageConverter();
 		converter.setSupportedMediaTypes(APPLICATION_JAVASCRIPT_MEDIA_TYPE);
-		restTemplate.getMessageConverters().add(converter);
-		return restTemplate;
+
+		return new RestTemplateBuilder()
+				.additionalMessageConverters(converter)
+				.additionalInterceptors(interceptor)
+				.requestFactory(() -> new BufferingClientHttpRequestFactory(new HttpComponentsClientHttpRequestFactory()))
+				.build();
 	}
 }
